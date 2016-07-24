557cc5cf6f28ec1be1702efcc5ff6e9a1e99ad7c
noelano <noelanorodriguez@gmail.com>
2016-07-24 19:14:21 +0100

    Generate new TDM for reuters data from the distances that the concept tree object provides

--- fca/LatticeFromTree.py
+++ fca/LatticeFromTree.py
@@ -1,6 +1,7 @@
 from ConceptTree import Concept, ConceptTree
 import datetime
 import numpy as np
+import pandas as pd
 
 input_file = "reuters_tree.json"
 print("Start time: " + str(datetime.datetime.now()))
@@ -15,7 +16,7 @@ for path in reuters.paths:
 
 print("Maximum path length: " + str(max))
 reuters.generateLattice_v2()
-print("Lattice complete: "  + str(datetime.datetime.now()))
+print("Lattice complete: " + str(datetime.datetime.now()))
 #reuters.visualiseLattice("reuters_graph", view=True)
 print(len(reuters.attributes))
 print(len(reuters.attribute_labels))
@@ -25,4 +26,28 @@ print(len(reuters.node_attributes))
 reuters.findNeighbours()
 reuters.generateDistances()
 print(reuters.distances.values)
-print("Distances complete: "  + str(datetime.datetime.now()))
+print("Distances complete: " + str(datetime.datetime.now()))
+
+input = "../DataEngineering/reuters_tdm.csv"
+f = open(input)
+vocab = f.readline()
+f.close()
+vocab_leng = len(vocab)
+proximity_matrix = np.array((vocab_leng, vocab_leng))
+
+max_sd = reuters.distances.values.max()
+for i in vocab:
+    node_i = reuters.attribute_labels[i]
+    for j in vocab:
+        node_j = reuters.attribute_labels[j]
+        proximity_matrix[i][j] = 1 - (reuters.distances[node_i][node_j] / max_sd)
+
+print(proximity_matrix)
+print("Proximities complete: " + str(datetime.datetime.now()))
+
+np.savetxt("reuters_proximities.csv", proximity_matrix, delimiter=',')
+X = pd.read_csv(input, delimiter=',')
+new_TDM = np.dot(X.values, proximity_matrix)
+
+print("New TDM complete: " + str(datetime.datetime.now()))
+np.savetxt("reuters_new_tdm.csv", new_TDM, delimiter=',')
